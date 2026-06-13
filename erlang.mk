fd27324363a5d9e558a19bc88108e05279d2afcd
Nick Vatamaniuc <vatamane@gmail.com>
2026-06-13 16:46:52 -0400

    use ASF mirror for cowlib 2.17.1

--- erlang.mk
+++ erlang.mk
@@ -312,7 +312,7 @@ pkg_cowlib_name = cowlib
 pkg_cowlib_description = Support library for manipulating Web protocols.
 pkg_cowlib_homepage = http://ninenines.eu
 pkg_cowlib_fetch = git
-pkg_cowlib_repo = https://github.com/ninenines/cowlib
+pkg_cowlib_repo = https://github.com/apache/couchdb-cowlib
 pkg_cowlib_commit = master
 
 PACKAGES += elixir
